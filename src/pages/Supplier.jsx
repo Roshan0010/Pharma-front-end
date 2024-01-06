bd59ddc8bc05c298460cf5699176e7263468797b
Roshan0010 <iamroshan6417@gmail.com>
2024-01-06 22:07:59 +0530

    adding gitignore file

---
+++ src/pages/Supplier.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ProgressBar from '../components/ProgressBar'
+
+const Supplier = () => {
+  return (
+    <div>
+    <div>
+
+    </div>
+    <ProgressBar/>
+    <div>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Supplier
